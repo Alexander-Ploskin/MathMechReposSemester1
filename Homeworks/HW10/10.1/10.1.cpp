9457aed4ee370b70f78644f9aad9061592ad8311
Alexander_Ploskin <Ploskin010&@gmail.com>
2019-11-19 23:58:20 +0300

    Не умею пользоваться гитом и сделал все за один коммит

---
+++ Homeworks/HW10/10.1/10.1.cpp
@@ -0,0 +1,92 @@
+﻿#include <stdio.h>
+#include <ctype.h>
+#include "HashTable.h"
+
+const int maxLength = 100;
+
+bool isPunctuationMark(char symbol)
+{
+	return symbol == ',' || symbol == '.' || symbol == '!' || symbol == '?' || symbol == '(' || symbol == ')' || symbol == ':';
+}
+
+bool test()
+{
+	FILE* filePtr = fopen("test.txt", "r");
+
+	HashTable* table = createTable();
+
+	while (!feof(filePtr))
+	{
+		char word[maxLength]{};
+		fscanf(filePtr, "%s", word);
+		char cleanWord[maxLength]{};
+		int i = 0;
+		int j = 0;
+		while (word[i] != '\0')
+		{
+			word[i] = tolower(word[i]);
+			if (!isPunctuationMark(word[i]))
+			{
+				cleanWord[j] = word[i];
+				j++;
+			}
+			i++;
+		}
+		table = addToTable(table, cleanWord, 1);
+	}
+
+	fclose(filePtr);
+
+	char string1[] = "basic";
+	char string2[] = "something";
+	bool test1 = containsInTable(table, string1);
+
+	removeFromeTable(table, string1);
+	bool test2 = containsInTable(table, string1);
+
+	bool test3 = containsInTable(table, string2);
+
+	deleteTable(table);
+
+	return true;
+}
+
+int main()
+{
+	if (!test())
+	{
+		return 1;
+	}
+
+	FILE* filePtr = fopen("input.txt", "r");
+
+	HashTable* table = createTable();
+	
+	while (!feof(filePtr))
+	{
+		char word[maxLength]{};
+		fscanf(filePtr, "%s", word);
+		char cleanWord[maxLength]{};
+		int i = 0;
+		int j = 0;
+		while (word[i] != '\0')
+		{
+			word[i] = tolower(word[i]);
+			if (!isPunctuationMark(word[i]))
+			{
+				cleanWord[j] = word[i];
+				j++;
+			}
+			i++;
+		}
+		table = addToTable(table, cleanWord, 1);
+	}
+
+	fclose(filePtr);
+
+	printTable(table);
+
+	deleteTable(table);
+
+	return 0;
+}
---
+++ Homeworks/HW10/10.1/List.cpp
@@ -0,0 +1,174 @@
+#include <string.h>
+#include <stdio.h>
+#include "List.h"
+
+struct ListElement
+{
+	char value[maxLength]{};
+	int amount = 1;
+	ListElement* next = nullptr;
+};
+
+struct List
+{
+	ListElement* head = nullptr;
+};
+
+List* createList()
+{
+	return new List;
+}
+
+bool isEmpty(List* list)
+{
+	return list->head == nullptr;
+}
+
+bool contains(List* list, char* value)
+{
+	ListElement* currentElement = list->head;
+	while (true)
+	{
+		if (currentElement == nullptr)
+		{
+			return false;
+		}
+		if (strcmp(currentElement->value, value) == 0)
+		{
+			return true;
+		}
+		currentElement = currentElement->next;
+	}
+}
+
+void addToList(List* list, char* value, int amount)
+{
+	if (isEmpty(list))
+	{
+		list->head = new ListElement;
+		list->head->amount = amount;
+		strcpy(list->head->value, value);
+		return;
+	}
+	ListElement* currentElement = list->head;
+	while (currentElement->next != nullptr && strcmp(value, currentElement->next->value) != 0)
+	{
+		currentElement = currentElement->next;
+	}
+	if (currentElement == list->head && strcmp(currentElement->value, value) == 0)
+	{
+		currentElement->amount++;
+		return;
+	}
+	if (currentElement->next == nullptr)
+	{
+		currentElement->next = new ListElement;
+		currentElement->next->amount = amount;
+		strcpy(currentElement->next->value, value);
+		return;
+	}
+	currentElement->next->amount++;
+}
+
+void removeFromList(List* list, char* value)
+{
+	if (isEmpty(list))
+	{
+		return;
+	}
+	if (strcmp(list->head->value, value) == 0)
+	{
+		if (list->head->amount > 1)
+		{
+			list->head->amount--;
+			return;
+		}
+		delete list->head;
+		list->head = nullptr;
+		return;
+	}
+	ListElement* currentElement = list->head;
+	while (currentElement->next != nullptr && strcmp(currentElement->next->value, value) != 0)
+	{
+		currentElement = currentElement->next;
+	}
+	if (currentElement->next != nullptr)
+	{
+		if (currentElement->next->amount > 1)
+		{
+			currentElement->next->amount--;
+			return;
+		}
+		ListElement* help = currentElement->next;
+		currentElement->next = currentElement->next->next;
+		delete help;
+		return;
+	}
+}
+
+void printList(List* list)
+{
+	if (isEmpty(list))
+	{
+		return;
+	}
+	ListElement* currentElement = list->head;
+	while (currentElement != nullptr)
+	{
+		printf("%s - %d\n", currentElement->value, currentElement->amount);
+		currentElement = currentElement->next;
+	}
+}
+
+int pop(List* list, char* buffer)
+{
+	if (isEmpty(list))
+	{
+		return 0;
+	}
+	if (list->head->next == nullptr)
+	{
+		int result = list->head->amount;
+		strcpy(buffer, list->head->value);
+		delete list->head;
+		list->head = nullptr;
+		return result;
+	}
+	ListElement* help = list->head->next;
+	strcpy(buffer, list->head->value);
+	int result = list->head->amount;
+	delete list->head;
+	list->head = help;
+	return result;
+}
+
+void deleteList(List* list)
+{
+	while (!isEmpty(list))
+	{
+		ListElement* help = list->head->next;
+		delete list->head;
+		list->head = help;
+	}
+	delete list;
+}
+
+int lengthOfList(List* list)
+{
+	if (isEmpty(list))
+	{
+		return 0;
+	}
+	int result = 0;
+	ListElement* currentElement = list->head;
+	while (true)
+	{
+		result++;
+		currentElement = currentElement->next;
+		if (currentElement == nullptr)
+		{
+			return result;
+		}
+	}
+}
+
