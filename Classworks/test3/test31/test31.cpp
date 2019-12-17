a42e1089c7ce07670dbe7d35eaf76db12134e55b
Alexander_Ploskin <Ploskin010&@gmail.com>
2019-12-17 15:11:43 +0300

    Первая задача

---
+++ Classworks/test3/test31/test31.cpp
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+#include "list.h"
+
+int main()
+{
+	List* list = createList();
+	int buffer = -1;
+	while (buffer != 0)
+	{
+		scanf("%d", &buffer);
+		if (!isContains(list, buffer))
+		{
+			push(list, buffer);
+		}
+	}
+	printList(list);
+	deleteList(list);
+	return 0;
+}
