c3841b6a3d5a1b03cab0b93a1890e4a7e2339d34
Alexander_Ploskin <Ploskin010&@gmail.com>
2019-12-17 13:55:05 +0300

    Надо переключить ветку

--- Homeworks/HW13/13.2/13.2.cpp
+++ Homeworks/HW13/13.2/13.2.cpp
@@ -4,6 +4,20 @@
 
 const int maxLength = 10000;
 
+bool stringCompare(char* string1, char* string2)
+{
+	const int lengthOfString1 = strlen(string1);
+
+	for (int i = 0; i < lengthOfString1; ++i)
+	{
+		if (string1[i] != string2[i])
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
 char* highlightComments(FILE* filePtr, int** fsmTable, char* header, int sizeOfHeader)
 {
 	char bufferString[maxLength]{};
@@ -38,17 +52,20 @@ bool test(int** fsmTable, char* header, int lengthOfStrings)
 	FILE* filePtr1 = fopen("test1.txt", "r");
 	char* buffer1 = highlightComments(filePtr1, fsmTable, header, lengthOfStrings);
 	fclose(filePtr1);
-	bool test1 = strcmp(buffer1, "comment");
+	char comments1[] = {'/','*','c','o','m','m','e','n','t','*','/' ,'\0' };
+	bool test1 = stringCompare(buffer1, comments1);
 	
 	FILE* filePtr2 = fopen("test2.txt", "r");
 	char* buffer2 = highlightComments(filePtr2, fsmTable, header, lengthOfStrings);
 	fclose(filePtr2);
-	bool test2 = strcmp(buffer2, "/**/");
+	char comments2[] = "/**/";
+	bool test2 = strcmp(buffer2, comments2) == 0;
 
 	FILE* filePtr3 = fopen("test3.txt", "r");
 	char* buffer3 = highlightComments(filePtr3, fsmTable, header, lengthOfStrings);
 	fclose(filePtr3);
-	bool test3 = strcmp(buffer3, "");
+	char comments3[] = "";
+	bool test3 = strcmp(buffer3, comments3) == 0;
 
 	return test1 && test2 && test3;
 }
@@ -70,6 +87,7 @@ int main()
 
 	if (!test(fsmTable, header, lengthOfStrings))
 	{
+		deleteTable(fsmTable, header, lengthOfStrings);
 		return 1;
 	}
 
