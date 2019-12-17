ff8993997a8927f160eec951172a293dd8ac174e
Alexander_Ploskin <Ploskin010&@gmail.com>
2019-12-17 15:18:43 +0300

    Теперь есть приглашение ко вводу

--- Classworks/test3/test31/test31.cpp
+++ Classworks/test3/test31/test31.cpp
@@ -5,10 +5,11 @@ int main()
 {
 	List* list = createList();
 	int buffer = -1;
+	printf("Enter the string\n");
 	while (buffer != 0)
 	{
 		scanf("%d", &buffer);
-		if (!isContains(list, buffer))
+		if (!isContains(list, buffer) && buffer != 0)
 		{
 			push(list, buffer);
 		}
