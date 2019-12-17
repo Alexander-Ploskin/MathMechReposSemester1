16f3fbb895d12041e2704c5f9394e536927a0a96
Alexander_Ploskin <Ploskin010&@gmail.com>
2019-12-17 19:19:47 +0300

    Без deleteTrash

--- Homeworks/HW13/13.2/13.2.cpp
+++ Homeworks/HW13/13.2/13.2.cpp
@@ -4,21 +4,6 @@
 
 const int maxLength = 10000;
 
-void deleteTrash(char* string)
-{
-	int i = 0;
-	while (string[i] != '\0')
-	{
-		i++;
-	}
-	i--;
-	while (string[i] == ' ' || string[i] == '\n')
-	{
-		string[i] = '\0';
-		i--;
-	}
-}
-
 char* highlightComments(FILE* filePtr, int** fsmTable, char* header, int sizeOfHeader)
 {
 	char bufferString[maxLength]{};
@@ -53,19 +38,16 @@ bool test(int** fsmTable, char* header, int lengthOfStrings)
 	FILE* filePtr1 = fopen("test1.txt", "r");
 	char* buffer1 = highlightComments(filePtr1, fsmTable, header, lengthOfStrings);
 	fclose(filePtr1);
-	deleteTrash(buffer1);
 	bool test1 = strcmp(buffer1, "/*comment***/") == 0;
 	
 	FILE* filePtr2 = fopen("test2.txt", "r");
 	char* buffer2 = highlightComments(filePtr2, fsmTable, header, lengthOfStrings);
 	fclose(filePtr2);
-	deleteTrash(buffer2);
 	bool test2 = strcmp(buffer2, "/**/") == 0;
 
 	FILE* filePtr3 = fopen("test3.txt", "r");
 	char* buffer3 = highlightComments(filePtr3, fsmTable, header, lengthOfStrings);
 	fclose(filePtr3);
-	deleteTrash(buffer3);
 	bool test3 = strcmp(buffer3, "/*/*/") == 0;
 
 	return test1 && test2 && test3;
