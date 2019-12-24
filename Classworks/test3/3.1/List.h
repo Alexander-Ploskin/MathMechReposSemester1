297e1ebd8886af808bcd67f9346e96fa07effbe0
Alexander_Ploskin <Ploskin010&@gmail.com>
2019-12-24 15:24:04 +0300

    Убрал лишние функции из хедера

--- Classworks/test3/3.1/List.h
+++ Classworks/test3/3.1/List.h
@@ -19,23 +19,11 @@ void deleteList(List* list);
 //Добавляет в список новое значение
 void add(List* list, Type value);
 
-//Удаялет значение из списка
-void remove(List* list, Type value);
-
 //Проверяет наличие значения в списке
 bool contains(List* list, Type value);
 
-//Проверяет равенство двух списков
-bool listCmp(List* list1, List* list2);
-
-//Заполняет список элементами из массива
-void fillListByArray(List* list, Type* array, int sizeOfArray);
-
 //Проверяет пустоту списка
 bool isEmpty(List* list);
-
-//Печатает список
-void printList(List* list);
 
 //Печатает список в файл
 void printListToFile(List* list, FILE* filePtr);
