820b4d78728198847cd30373967e5a0f16977a2e
Alexander_Ploskin <Ploskin010&@gmail.com>
2019-12-24 15:31:08 +0300

    Перепроверил

--- Classworks/test3/3.2/3.2.cpp
+++ Classworks/test3/3.2/3.2.cpp
@@ -5,11 +5,11 @@ int main()
 {
 	PriorityQueue* queue = createQueue();
 
-	enqueue(queue, 5, 3);
-	enqueue(queue, 4, 4);
+	enqueue(queue, 4, 3);
+	enqueue(queue, 3, 4);
 	enqueue(queue, 2, 7);
-	enqueue(queue, 10, 10);
-	enqueue(queue, 15, 0);
+	enqueue(queue, 1, 10);
+	enqueue(queue, 5, 0);
 
 	printf("%d\n", dequeue(queue));
 	printf("%d\n", dequeue(queue));
