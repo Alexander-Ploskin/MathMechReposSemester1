d53a809a54ef7051d0e350236bd7666ccfbf68a6
Alexander_Ploskin <Ploskin010&@gmail.com>
2019-12-25 11:56:28 +0300

    Написал ДКА

--- Classworks/test3/3.3/3.3.cpp
+++ Classworks/test3/3.3/3.3.cpp
@@ -1,20 +1,47 @@
-﻿// 3.3.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <stdio.h>
+#include "Analyzer.h"
 
-#include <iostream>
+bool test()
+{
+	char test1String[] = "17B10-mm";
+	char test2String[] = "17B11-mm";
+	char test3String[] = "17B1-mm";
+	char test4String[] = "17B10-m";
+	char test5String[] = "1B10-mm";
+	char test6String[] = "17N10-mm";
+	char test7String[] = "ireug93875";
+
+	bool test1 = isNumberOfGroup(test1String);
+	bool test2 = !isNumberOfGroup(test2String);
+	bool test3 = isNumberOfGroup(test3String);
+	bool test4 = !isNumberOfGroup(test4String);
+	bool test5 = !isNumberOfGroup(test5String);
+	bool test6 = !isNumberOfGroup(test6String);
+	bool test7 = !isNumberOfGroup(test7String);
+
+	return test1 && test2 && test3 && test4 && test5 && test6 && test7;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	if (!test())
+	{
+		return 1;
+	}
+
+	printf("Enter the string shorter the 10 symbols:\n");
+
+	char buffer[maxLength]{};
+	scanf("%s", &buffer);
+
+	if (isNumberOfGroup(buffer))
+	{
+		printf("Correct!\n");
+	}
+	else
+	{
+		printf("Incorrect!\n");
+	}
+
+	return 0;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
