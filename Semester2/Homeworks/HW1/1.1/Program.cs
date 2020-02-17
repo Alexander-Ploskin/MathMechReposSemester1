8dc211b0cf186bc884bff7ea40a831b7281ee035
Alexander_Ploskin <Ploskin010&@gmail.com>
2020-02-17 19:43:56 +0300

    Первая задача

---
+++ Semester2/Homeworks/HW1/1.1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        public static int CalculatingOfFactorial(int inputNumber)
+        {
+            if (inputNumber < 2)
+            {
+                return 1;
+            }
+
+            return inputNumber * CalculatingOfFactorial(inputNumber - 1);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number:");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Factorial of {number} is {CalculatingOfFactorial(number)}");
+        }
+    }
+}
